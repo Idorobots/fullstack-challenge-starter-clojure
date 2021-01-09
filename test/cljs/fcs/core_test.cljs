98b0ec262d221b2df51edbb3111e61f635bab864
Kajetan Rzepecki <k@spartan.works>
2021-01-09 02:50:24 +0100

    Added re-frame-specific fixture to the FE tests.

--- test/cljs/fcs/core_test.cljs
+++ test/cljs/fcs/core_test.cljs
@@ -1,8 +1,18 @@
 (ns fcs.core-test
   (:require [cljs.test :refer-macros [is are deftest testing use-fixtures]]
             [pjstadig.humane-test-output]
+            [re-frame.core :as rf]
             [reagent.core :as reagent :refer [atom]]
             [fcs.core :as rc]))
 
+(defn fixture-re-frame []
+  (let [restore-re-frame (atom nil)]
+    {:before #(reset! restore-re-frame (rf/make-restore-fn))
+     :after  #(@restore-re-frame)}))
+
+(use-fixtures :each (fixture-re-frame))
+
 (deftest test-home
-  (is (= true true)))
+  (testing "sanity"
+    (println "test")
+    (is (= true true))))
